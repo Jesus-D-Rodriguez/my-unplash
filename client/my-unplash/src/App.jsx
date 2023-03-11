b28d494fc946b767237ff622dbd68f25dff7c899
jesus <jdrch62@gmail.com>
2023-03-11 16:57:50 -0400

    App finished

--- client/my-unplash/src/App.jsx
+++ client/my-unplash/src/App.jsx
@@ -1,30 +1,50 @@
 import React, {useEffect, useState} from "react";
 import Navbar from "./components/Navbar/Navbar";
+import ImagesContainer from "./components/ImagesContainer/ImagesContainer";
 import "./App.css"
+import Formulario from "./components/Form/Form";
 
 function App() {
 
-  const [backendData, setBackendData] = useState([{}]); 
-  useEffect(()=>{
-    fetch("http://localhost:5000/a").then(
+  const [searchTerm, setSearchTerm] = useState("");
+  const [buscado, setBuscado] = useState(false);
+
+  function handleSearchTermChange(term) {
+    setSearchTerm(term);
+    setBuscado(true);
+    console.log(searchTerm);
+  }
+
+  function handleBuscado() {
+
+  }
+
+  /*{(typeof backendData.users === "undefined") ? (
+    <p>Loading...</p>
+  ): (
+    backendData.users.map((user, i) => (
+      <p key={i}>{user}</p>
+    ))
+  )}*/
+
+
+
+  /*const [images, setImages] = useState([]); 
+  useEffect(async ()=>{
+    await fetch("http://localhost:5000/").then(
       response => response.json()
     ).then(
       data=> {
-        setBackendData(data);
+        setImages(data);
       }
     );
-  }, [])
+    console.log(images);
+  }, [])*/
 
   return (
     <div className="container">
-      <Navbar/>
-      {(typeof backendData.users === "undefined") ? (
-        <p>Loading...</p>
-      ): (
-        backendData.users.map((user, i) => (
-          <p key={i}>{user}</p>
-        ))
-      )}
+      <Navbar onSearchTermChange={handleSearchTermChange}/>
+      <ImagesContainer searchTerm={searchTerm} buscado={buscado}/>
     </div>
   )
 }
